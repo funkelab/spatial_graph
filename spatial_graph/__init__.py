2f3e309e6854b8002e591dfae5fb949f5930af38
Talley Lambert <talley.lambert@gmail.com>
2024-11-07 13:50:04 -0500

    lint

--- spatial_graph/__init__.py
+++ spatial_graph/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version('spatial_graph')
+    __version__ = version("spatial_graph")
 except PackageNotFoundError:
     __version__ = "unknown"
 
