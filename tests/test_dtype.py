d478162c9bbe2cbaa79230298b774ea281593258
Talley Lambert <talley.lambert@gmail.com>
2025-04-13 10:47:45 -0400

    lint

--- tests/test_dtype.py
+++ tests/test_dtype.py
@@ -18,7 +18,7 @@ def test_dtype(base: str, size: int | None) -> None:
     assert dtype.to_c_decl("test") == f"{c_base} test" + (f"[{size}]" if size else "")
 
     assert dtype.to_pyxtype() == c_base + (f"[{size}]" if size else "")
-    assert dtype.to_pyxtype(use_memory_view=True) == c_base + (f"[::1]" if size else "")
+    assert dtype.to_pyxtype(use_memory_view=True) == c_base + ("[::1]" if size else "")
     assert dtype.to_pyxtype(add_dim=True) == c_base + ("[:, ::1]" if size else "[::1]")
     assert dtype.to_pyxtype(use_memory_view=True, add_dim=True) == c_base + (
         "[:, ::1]" if size else "[::1]"
